037d246f71b8b6df47c8f849785ae8801990082d
elyudwo <dudqk9696@naver.com>
2024-04-02 12:27:45 +0900

    [Refactor] : 스웨거 설명 변경

--- webclient/src/main/java/io/kr/inu/webclient/api/video/VideoController.java
+++ webclient/src/main/java/io/kr/inu/webclient/api/video/VideoController.java
@@ -18,7 +18,7 @@ public class VideoController {
 
     private final VideoService videoService;
 
-    @Operation(summary = "동영상 저장", description = "동영상 정보를 보내주세요옹.")
+    @Operation(summary = "동영상 저장", description = "JWT를 헤더에 보내주세요. multipart로 보내주실때 key 값에 'video' 로 보내주세요")
     @PostMapping("/v1/upload/video")
     public String uploadImage(UserEmail email, @RequestPart(name = "video", required = false) MultipartFile video) throws IOException {
         return videoService.uploadVideo(video, email.getEmail());
