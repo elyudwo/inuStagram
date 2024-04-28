56ad53bfb701f1a0f333c2562d5a7d1ad34db789
elyudwo <dudqk9696@naver.com>
2024-04-28 16:32:24 +0900

    [Refactor] : ELB 변경 후 CD 테스트

--- webclient/src/main/java/io/kr/inu/webclient/api/resolver/UserIdArgumentResolver.java
+++ webclient/src/main/java/io/kr/inu/webclient/api/resolver/UserIdArgumentResolver.java
@@ -3,6 +3,7 @@ package io.kr.inu.webclient.api.resolver;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Value("${jwt.secret}")
@@ -28,12 +30,15 @@ public class UserIdArgumentResolver implements HandlerMethodArgumentResolver {
                                      NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         try {
             String jwtToken = webRequest.getHeader("Authorization");
-            System.out.println("토큰입니다 : " + jwtToken);
+            log.info("토큰입니다 : " + jwtToken);
             if (jwtToken == null) {
-                return new UserEmail(null);
+                log.info("야 토큰이 없대");
+                return new UserEmail("sample@inu.ac.kr");
             }
             jwtToken = jwtToken.replace("Bearer ", "");
             String email = (JWT.require(Algorithm.HMAC512(SECRET)).build().verify(jwtToken).getClaim("email")).asString();
+
+            log.info("이메일입니다 : " + email);
             return new UserEmail(email);
         } catch (Exception e) {
             return null;
