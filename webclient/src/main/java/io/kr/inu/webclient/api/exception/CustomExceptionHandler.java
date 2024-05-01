eb19b23dcc5056597a7b4666173a36d44d9e544c
elyudwo <dudqk9696@naver.com>
2024-05-02 00:41:55 +0900

    [Refactor] : 유해 영상 예외 반환시 Http 상태 코드 400으로 추가

--- webclient/src/main/java/io/kr/inu/webclient/api/exception/CustomExceptionHandler.java
+++ webclient/src/main/java/io/kr/inu/webclient/api/exception/CustomExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.kr.inu.webclient.api.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,6 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handle(IllegalArgumentException e) {
-        return ResponseEntity.ok(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
