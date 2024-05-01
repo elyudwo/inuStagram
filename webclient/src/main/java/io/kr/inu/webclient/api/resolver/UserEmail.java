4a58203878aace227acc1a9171029886d8ecb16c
elyudwo <dudqk9696@naver.com>
2024-05-01 20:24:30 +0900

    [Feat] : 스웨거 헤더 설정 추가

--- webclient/src/main/java/io/kr/inu/webclient/api/resolver/UserEmail.java
+++ webclient/src/main/java/io/kr/inu/webclient/api/resolver/UserEmail.java
@@ -1,10 +1,12 @@
 package io.kr.inu.webclient.api.resolver;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Hidden
 public class UserEmail {
 
     private final String email;
