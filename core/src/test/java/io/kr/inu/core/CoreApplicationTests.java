08ef0e34387884454bbf03d4593d075137d010f5
elyudwo <dudqk9696@naver.com>
2024-05-27 15:08:52 +0900

    [Refactor] : 테스트 환경에 yml 스캔 메서드 추가

--- core/src/test/java/io/kr/inu/core/CoreApplicationTests.java
+++ core/src/test/java/io/kr/inu/core/CoreApplicationTests.java
@@ -1,10 +1,20 @@
 package io.kr.inu.core;
 
+import io.kr.inu.infra.redis.RedisConfig;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 class CoreApplicationTests {
+
+    @BeforeAll
+    static void setup() {
+        System.setProperty("spring.config.name", "application-core,application-infra");
+    }
 
     @Test
     void contextLoads() {
