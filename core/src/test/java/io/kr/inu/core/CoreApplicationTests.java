74808d4dde09d9685e77cee893676b2b0df370ed
elyudwo-devCra <skylee@devcra.com>
2024-11-11 11:11:34 +0900

    [Feat]: implement video views scheduler

--- core/src/test/java/io/kr/inu/core/CoreApplicationTests.java
+++ core/src/test/java/io/kr/inu/core/CoreApplicationTests.java
@@ -1,12 +1,8 @@
 package io.kr.inu.core;
 
-import io.kr.inu.infra.redis.RedisConfig;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 class CoreApplicationTests {
